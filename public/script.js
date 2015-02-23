e6552e9b5df40c82bd3940c1e8c50538d6e3d4ba
Nick Webster <nick.webster@qlikview.com>
2015-02-23 15:28:54 +0000

    Initial push for Qlik Sense and NodeJS integration

---
+++ public/script.js
@@ -0,0 +1,28 @@
+var config = {
+	host: "",
+	prefix: "",
+	port: 80,
+	isSecure: window.location.protocol === "https:"
+};
+
+var appId="";
+var objectId="";
+
+  require.config( {
+    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port: "") + config.prefix + "resources"
+  } );
+	require( ["js/qlik"], function ( qlik ) {
+	  qlik.setOnError( function ( error ) {
+	    alert( error.message );
+	  } );
+	  var app = qlik.openApp(appId, config);
+	  app.getObject("#qv1", objectId);
+	} );
+
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var urlParser = require('url');
+var qlikAuth = require('qlik-auth');
+
+var contentTypeMap = {
+  "js":"text/javascript",
+  "html":"text/html",
+  "css":"text/css",
+  "ttf":"font/ttf"
+};
+
+var server = http.createServer(function(req, res){
+  switch(req.url){
+    case "/":
+      serveFile("public/index.html", res);
+      break;
+    default:
+      if(req.url.indexOf('authenticate?')!=-1){
+        var profile = {
+          'UserDirectory': 'QLIK',
+          'UserId': 'sample',
+          'Attributes': []
+        }
+
+        var options = {
+         'Certificate': './client.pfx',
+         'PassPhrase': ''
+        }
+        qlikAuth.requestTicket(req, res, profile, options);
+      }
+      else{
+        serveFile(req.url.replace(/\//,''), res);
+      }
+      break;
+  }
+}).listen(3000);
+
+console.log('Server running on port 3000');
+
+function serveFile(url, res){
+  console.log(url.replace(/\//, ''));
+  var contentType = contentTypeMap[url.split('.').pop()];
+  console.log(contentType);
+  fs.readFile(url, function(err, data){
+    res.writeHead(200, {'Content-Type':contentType});
+    res.write(data);
+    res.end();
+  })
+}
